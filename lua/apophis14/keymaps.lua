a670190b6205c7cd17fe481748fa5c819c5ee5cd
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2022-09-16 23:42:33 -0300

    Keymaps

---
+++ lua/apophis14/keymaps.lua
@@ -0,0 +1,10 @@
+-- Alias Macros
+local map = vim.api.nvim_set_keymap
+local default_ops = {noremap = true, silent = true}
+local cmd = vim.cmd
+
+-- Keys remapping
+map('', '<up>','<nop>',{noremap = true})
+map('', '<down>','<nop>',{noremap = true})
+map('', '<left>','<nop>',{noremap = true})
+map('', '<right>','<nop>',{noremap = true})
