85184afed92125334029e447f178a721a6773e99
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2022-10-16 00:22:34 -0300

    Update. Indent and colorscheme

--- lua/apophis14/settings.lua
+++ lua/apophis14/settings.lua
@@ -12,7 +12,28 @@ g.mapleader = ' '
 opt.number         = true -- Line numbers
 opt.relativenumber = true -- Relative line numbers
 
+-- Indent
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.smartindent = true
+opt.wrap = true
+
 -- Color Theme
 g.gruvbox_contrast_dark = "hard"
-cmd('colorscheme gruvbox')
+
+g.everforest_enable_italic = 1
+g.everforest_transparent_background = 1
+g.everforest_ui_contrast = 'high'
+g.everforst_better_performance = 1
+
+g.onedark_termcolors = 256
+g.onedark_terminal_italics = 1
+
+cmd('colorscheme onedark')
 cmd('set background=dark')
+
+-- Indent Guides
+g.indent_guides_enable_on_vim_startup = 1
+
